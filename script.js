cbae37325773826e0323fec077496a8168a1d5b5
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-30 13:58:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let num1=Number(prompt("Digite um número"))
+let num2=Number(prompt("Digite outro número"))
+
+if (num1==num2) {
+    alert("Os números são iguais digite outro")
+}else if (num1>num2) {
+    alert("O maior valor é "+num1)
+}else{
+    alert("O maior valor é "+num2)
+}
